75c3f303f91b9ddbf2f3ddd1a5327744d2ef2c5c
Mark Craig <mark.craig@forgerock.com>
2017-03-16 13:14:25 +0100

    AME-12851 Move samples to community project location
    This patch prepares the move.

--- src/main/java/org/forgerock/openam/examples/quotaexhaustionaction/SampleQuotaExhaustionAction.java
+++ src/main/java/org/forgerock/openam/examples/quotaexhaustionaction/SampleQuotaExhaustionAction.java
@@ -1,27 +1,25 @@
 /*
- * Copyright 2012-2013 ForgeRock, Inc.
+ * Copyright 2012-2017 ForgeRock AS. All Rights Reserved
  *
- * The contents of this file are subject to the terms of the Common Development and
- * Distribution License (the License). You may not use this file except in compliance with the
- * License.
- *
- * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
- * specific language governing permission and limitations under the License.
- *
- * When distributing Covered Software, include this CDDL Header Notice in each file and include
- * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
- * Header, with the fields enclosed by brackets [] replaced by your own identifying
- * information: "Portions copyright [year] [name of copyright owner]".
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
  */
 package org.forgerock.openam.examples.quotaexhaustionaction;
 
-import com.iplanet.dpro.session.Session;
+import static org.forgerock.openam.session.SessionConstants.SESSION_DEBUG;
+import com.google.inject.Key;
+import com.google.inject.name.Names;
 import com.iplanet.dpro.session.SessionException;
 import com.iplanet.dpro.session.SessionID;
 import com.iplanet.dpro.session.service.InternalSession;
 import com.iplanet.dpro.session.service.QuotaExhaustionAction;
-import com.iplanet.dpro.session.service.SessionService;
 import com.sun.identity.shared.debug.Debug;
+import org.forgerock.guice.core.InjectorHolder;
+import org.forgerock.openam.session.Session;
+import org.forgerock.openam.session.clientsdk.SessionCache;
+
+import javax.inject.Inject;
 import java.util.Map;
 
 /**
@@ -30,7 +28,18 @@ import java.util.Map;
  */
 public class SampleQuotaExhaustionAction implements QuotaExhaustionAction {
 
-    private static Debug debug = SessionService.sessionDebug;
+    private static Debug debug = InjectorHolder.getInstance(Key.get(Debug.class, Names.named(SESSION_DEBUG)));
+
+    private final SessionCache sessionCache;
+
+    public SampleQuotaExhaustionAction() {
+        this.sessionCache = InjectorHolder.getInstance(SessionCache.class);
+    }
+
+    @Inject
+    public SampleQuotaExhaustionAction(SessionCache sessionCache) {
+        this.sessionCache = sessionCache;
+    }
 
     /**
      * Check if the session quota for a given user has been exhausted and
@@ -49,11 +58,10 @@ public class SampleQuotaExhaustionAction implements QuotaExhaustionAction {
             Map<String, Long> existingSessions) {
         for (Map.Entry<String, Long> entry : existingSessions.entrySet()) {
             try {
-                // Get an actual Session instance based on the session ID.
-                Session session =
-                        Session.getSession(new SessionID(entry.getKey()));
-                // Use the session to destroy itself.
-                session.destroySession(session);
+                // Get a Session from the cache based on the session ID, and destroy it.
+                SessionID sessionId = new SessionID(entry.getKey());
+                Session session = sessionCache.getSession(sessionId);
+                session.destroySession(sessionId);
                 // Only destroy the first session.
                 break;
             } catch (SessionException se) {
